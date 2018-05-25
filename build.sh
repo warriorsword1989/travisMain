c4e29bb0ecb211e186095b3c4bce28da1cbea1d2
warriorsword1989 <linglong@navinfo.com>
2018-05-25 15:35:21 +0800

    '测试traivs'

--- build.sh
+++ build.sh
@@ -6,7 +6,8 @@ git clone https://github.com/NavinfoWeb/Tollgate.git
 cd Tollgate
 npm install
 npm run-script build
-cp -r dist ../../dist
+ls
+cp -r dist ../dist
 echo "extart files start"
 cd ..
 mkdir -p build
